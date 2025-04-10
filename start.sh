18eba341b221547a63b60eb8fbbf4073d27056ce
davide <62804472+rgbbeard@users.noreply.github.com>
2025-04-10 08:07:57 +0200

    [upgrade] added console title

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 base="$(dirname $(readlink -f $0))"
 . "$base/.utils/cmdexists.sh"
 
+echo -en "\033]0;oc.console\a"
+
 if [[ $(cmdexists "python3") -eq 0 ]]; then
 	python3 /opt/oc.console/oc.console.py
 elif [[ $(cmdexists "python") -eq 0 ]]; then
